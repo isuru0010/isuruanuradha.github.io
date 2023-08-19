0129fbafd0f5c102b2dfaf0a7484d9821ad23393
isuru0010 <isuruanuradha260@gmail.com>
2023-08-19 13:56:57 +0530

    Hello This is my portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+
+window.addEventListener ("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle ("sticky", window. scrollY > 0);
+});
+
+let menu= document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+menu.onclick=()=>{
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+};
+window.onscroll = ()=>{
+    menu.classList.remove('bx-x');
+    navlist.classList.remove('active');
+};
